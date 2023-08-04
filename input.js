7d4d291217f2d4e5f56231672496e0b356398c68
Jack Tran <bluestar113311@gmail.com>
2023-08-04 16:16:51 -0700

    Refactored input.js and play.js to allow server to read user input and move the snake

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +15,22 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+
+  if (key === '\u0061') {
+    connection.write("Move: left");
+  }
+  
+  if (key === '\u0064') {
+    connection.write("Move: right");
+  }
+  
+  if (key === '\u0077') {
+    connection.write("Move: up");
+  }
+  
+  if (key === '\u0073') {
+    connection.write("Move: down");
   }
 };
 
